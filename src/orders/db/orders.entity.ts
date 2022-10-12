5cc0a8b023a374ca2acf5865eedec0a2968e3930
Michal Plewa <themipsial@gmail.com>
2022-10-12 03:12:02 +0200

    db for orders

---
+++ src/orders/db/orders.entity.ts
@@ -0,0 +1,68 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinTable,
+  OneToMany,
+  ManyToOne,
+} from 'typeorm';
+
+import { Tag } from './orderTag.entity';
+import { OrderedProducts } from './orderedProducts.entity';
+
+@Entity({ name: 'orders' })
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+
+  @OneToMany(
+    (type) => OrderedProducts,
+    (orderedProducts) => orderedProducts.orderId,
+  )
+  @JoinTable({
+    name: 'orderedProducts',
+    joinColumn: {
+      name: 'orderId',
+    },
+    inverseJoinColumn: {
+      name: 'productId',
+    },
+  })
+  orderItems: OrderedProducts[];
+
+  @JoinTable({
+    name: 'state',
+    joinColumn: {
+      name: 'orderId',
+    },
+    inverseJoinColumn: {
+      name: 'tagId',
+    },
+  })
+  state: Tag[];
+
+  @JoinTable({
+    name: 'orderedAddresses',
+    joinColumn: {
+      name: 'orderId',
+    },
+    inverseJoinColumn: {
+      name: 'addressId',
+    },
+  })
+  address: string;
+
+  @Column({ type: 'float' })
+  price: number;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+}
